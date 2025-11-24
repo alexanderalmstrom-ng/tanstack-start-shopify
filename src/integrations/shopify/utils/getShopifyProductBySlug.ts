42c2af3239e64be7d0ed3f97d11f9a26b3ea8c65
Alexander Almström <info@alexanderalmstrom.com>
2025-11-24 18:40:39 +0100

    Return response

--- src/integrations/shopify/utils/getShopifyProductBySlug.ts
+++ src/integrations/shopify/utils/getShopifyProductBySlug.ts
@@ -2,5 +2,7 @@ import shopifyClient from "../client";
 import productBySlugQuery from "../lib/product/productBySlug.query";
 
 export async function getShopifyProductBySlug(slug: string) {
-  return (await shopifyClient(productBySlugQuery, { slug })).data?.product;
+  const response = await shopifyClient(productBySlugQuery, { slug });
+
+  return response.data?.product;
 }
